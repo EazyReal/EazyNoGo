1776b8e98ac510da96a4e14cbfc6b38c12898b18
yt lin <0312fs3@gmail.com>
2019-10-21 11:36:36 +0800

    clang formatter

--- EazyNoGo/Agent/eznogo-rave-classic-ver/mcts.cpp
+++ EazyNoGo/Agent/eznogo-rave-classic-ver/mcts.cpp
@@ -4,115 +4,120 @@
 
 void MCTS::init_with_board(board b)
 {
-  root = new Node;
-  root_board = b; //copy a board
-  root->initNode(NULL, BOARDVL, root_board.just_play_color());//2019.10.10.17:09....
-  //use NULL, BOARDSZ, 0, 0 for last action doesnt matter
-  root->expand(root_board);
-  total = 0;
+    root = new Node;
+    root_board = b; //copy a board
+    root->initNode(NULL, BOARDVL, root_board.just_play_color()); //2019.10.10.17:09....
+    //use NULL, BOARDSZ, 0, 0 for last action doesnt matter
+    root->expand(root_board);
+    total = 0;
 }
 
 void MCTS::reset()
 {
-  simu_board = root_board; //reset select simulation board(bpath, wpath will be rested)
-  path.clear(); //clear select path
-  rave_path[0].clear();
-  rave_path[1].clear();
-  simu_boardb.bpsize=0;
-	simu_boardb.wpsize=0;
+    simu_board = root_board; //reset select simulation board(bpath, wpath will be rested)
+    path.clear(); //clear select path
+    rave_path[0].clear();
+    rave_path[1].clear();
+    simu_boardb.bpsize = 0;
+    simu_boardb.wpsize = 0;
 }
 
 Node* MCTS::select(Node* r)
 {
-  Node* cur = r;
-  path.push_back(cur);
-  while((cur->cnt > BASENUM||cur = r)  && cur->cptr.size()) //if cptr.size() = 0, not expanded or game over
-  {
-    cur = cur->best_child();
-    simu_board.add(cur->pos, cur->color);
-    rave_path[cur->color].push_back(cur->pos);
+    Node* cur = r;
     path.push_back(cur);
-  }
-  return cur;
+    while ((cur->cnt > BASENUM || cur = r) && cur->cptr.size()) //if cptr.size() = 0, not expanded or game over
+    {
+        cur = cur->best_child();
+        simu_board.add(cur->pos, cur->color);
+        rave_path[cur->color].push_back(cur->pos);
+        path.push_back(cur);
+    }
+    return cur;
 }
 
 bool MCTS::roll_out() //the one in the board
 {
-  bool c = !simu_board.just_play_color(); //c = cur player to move
-  //getv update info for rollout by pointer and reference
-  simu_board.getv(bpos, wpos, bothpos, bpos_sz, wpos_sz, bothpos_sz);
-  //simulate the rest of board, record is in b
-  //res == 1 => black(0)player win
-  int res = simu_board.simulate(c, bpos, wpos, bothpos, bpos_sz, wpos_sz, bothpos_sz);
-  //update the simu actions to my rave_path vec
-  for(int i = 0; i < simu_board.bpsize; i++)
-    rave_path[0].push_back(simu_board.bpath[i]);
-  for(int i = 0; i < simu_board.wpsize; i++)
-    rave_path[1].push_back(simu_board.wpath[i]); //bugged until 2019/10/0 18:22
-  return (res == -1);
+    bool c = !simu_board.just_play_color(); //c = cur player to move
+    //getv update info for rollout by pointer and reference
+    simu_board.getv(bpos, wpos, bothpos, bpos_sz, wpos_sz, bothpos_sz);
+    //simulate the rest of board, record is in b
+    //res == 1 => black(0)player win
+    int res = simu_board.simulate(c, bpos, wpos, bothpos, bpos_sz, wpos_sz, bothpos_sz);
+    //update the simu actions to my rave_path vec
+    for (int i = 0; i < simu_board.bpsize; i++)
+        rave_path[0].push_back(simu_board.bpath[i]);
+    for (int i = 0; i < simu_board.wpsize; i++)
+        rave_path[1].push_back(simu_board.wpath[i]); //bugged until 2019/10/0 18:22
+    return (res == -1);
 }
 
 void MCTS::backpropogation(bool res)
 {
-  //update the sim_tree part with path, and child's rave value with rave_path
-  //rave_path[0/1][i] ~= path[i*2]
-  for(int t = 0; t < path.size(); t++)
-	{
-		path[t]->update(res);
-    bool c = path[t]->color;
-    //update subtree action value AMAF
-    for(int tp = 0; tp < rave_path[c].size(); tp++) //the former play matters more in nogo
-    {
-      if(rave_path[c][tp] == path[t]->pos) path[t]->rave_update(res);
+    //update the sim_tree part with path, and child's rave value with rave_path
+    //rave_path[0/1][i] ~= path[i*2]
+    for (int t = 0; t < path.size(); t++) {
+        path[t]->update(res);
+        bool c = path[t]->color;
+        //update subtree action value AMAF
+        for (int tp = 0; tp < rave_path[c].size(); tp++) //the former play matters more in nogo
+        {
+            if (rave_path[c][tp] == path[t]->pos)
+                path[t]->rave_update(res);
+        }
     }
-	}
 }
 
 Action MCTS::best_action(board init_b, bool color, int simu_per_step)
 {
-  init_with_board(init_b);
-  time_t start_t, cur_t;
-  start_t = clock();
-  cur_t = clock();
+    init_with_board(init_b);
+    time_t start_t, cur_t;
+    start_t = clock();
+    cur_t = clock();
 
-  for(int ep = 0; ep < simu_per_step; ep++){
-      reset();
-      //selection
-      Node* selected_root = select(root);
-      //expansion
-      bool res;
-      int nc = selected_root->expand(simu_board);
-      if(nc == 0)
-      {
-        res = simu_board.just_play_color();
-      }
-      else{
-        Node* cur = selected_root->best_child();
-        simu_board.add(cur->pos, cur->color);
-        rave_path[cur->color].push_back(cur->pos);
-        path.push_back(cur);
-        //simulation
-        res = roll_out();
-      }
-      //backpropogation
-      backpropogation(res);
-  }
-  //return result, forget to judge NULL at first
-  Node *best_node = root->best_child();
-  Action maxA = (best_node == NULL) ? -1 : best_node->pos;
+    for (int ep = 0; ep < simu_per_step; ep++) {
+        reset();
+        //selection
+        Node* selected_root = select(root);
+        //expansion
+        bool res;
+        int nc = selected_root->expand(simu_board);
+        if (nc == 0) {
+            res = simu_board.just_play_color();
+        }
+        else {
+            Node* cur = selected_root->best_child();
+            simu_board.add(cur->pos, cur->color);
+            rave_path[cur->color].push_back(cur->pos);
+            path.push_back(cur);
+            //simulation
+            res = roll_out();
+        }
+        //backpropogation
+        backpropogation(res);
+    }
+    //return result, forget to judge NULL at first
+    Node* best_node = root->best_child();
+    Action maxA = (best_node == NULL) ? -1 : best_node->pos;
 #ifdef USETIME
-  assert(cur_t < start_t + time_limit + time_limit); //for no inf loop;
+    assert(cur_t < start_t + time_limit + time_limit); //for no inf loop;
 #endif
-  clear();
-  return maxA;
+    clear();
+    return maxA;
 }
 
 double MCTS::calc_winrate(board b, bool color)
 {
-  return 0.5;
+    return 0.5;
 }
 
 void MCTS::clear()
 {
-  delete root;
+    delete root;
 }
+
+
+Format!Style:
+C++ online code formatter © 2014 by KrzaQ
+
+Powered by vibe.d, the D language and clang-format
--- EazyNoGo/main.cpp
+++ EazyNoGo/main.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <ctime> //clock = long, second =  clock()/CLOCKS_PER_SEC
 #include <cassert>
-#include<iomanip>
+#include <iomanip>
 
 //#define DEBUG
 
@@ -28,114 +28,140 @@ using namespace std;
 //protocol transformation
 string Int2SGF(int i)
 {
-	string s = "aa";
-	s[0] += (i/BOARDSZ);
-	s[1] += (i%BOARDSZ);
-	return s;
+    string s = "aa";
+    s[0] += (i / BOARDSZ);
+    s[1] += (i % BOARDSZ);
+    return s;
 }
 
 int GTP2Int(string s)
 {
-	if(s[0]>='a'&&s[0]<='z')
-	{
-		int x = s[0]-'a';
-		if(s[0]>'i') x--;
-		int y = s[1]-'1';
-		return x*BOARDSZ+y;
-	}else{
-		int x = s[0]-'A';
-		if(s[0]>'I')x--;
-		int y = s[1]-'1';
-		return x*BOARDSZ+y;
-	}
+    if (s[0] >= 'a' && s[0] <= 'z') {
+        int x = s[0] - 'a';
+        if (s[0] > 'i')
+            x--;
+        int y = s[1] - '1';
+        return x * BOARDSZ + y;
+    }
+    else {
+        int x = s[0] - 'A';
+        if (s[0] > 'I')
+            x--;
+        int y = s[1] - '1';
+        return x * BOARDSZ + y;
+    }
 }
 
 string Int2GTP(int i)
 {
-	string s = "A1";
-    s[0]+= i / BOARDSZ;
-    if(s[0]>='I')s[0]++;
-    s[1]+= i % BOARDSZ;
+    string s = "A1";
+    s[0] += i / BOARDSZ;
+    if (s[0] >= 'I')
+        s[0]++;
+    s[1] += i % BOARDSZ;
     return s;
 }
 
-string known_commands[11] =
-{
-	"protocol_version",
-	"name",
-	"known_command",
-	"list_commands",
-	"quit",
-	"boardsize",
-	"clear_board",
-	"komi",
-	"play",
-	"genmove",
-	"reg_genmove"
+string known_commands[11] = {
+    "protocol_version",
+    "name",
+    "known_command",
+    "list_commands",
+    "quit",
+    "boardsize",
+    "clear_board",
+    "komi",
+    "play",
+    "genmove",
+    "reg_genmove"
 };
 
 int main(int argc, char** argv)
 {
-  //some setting, initialization
-	MCTS agent;
-	board env;
-	env.clear();
-	srand(time(NULL)) ;
-	int simu_per_step = DEFAUT_SIMS;
-	if(argc == 2) istringstream(string(argv[1])) >> simu_per_step;
-	cerr << "simulate " << simu_per_step << "/step" << endl;
-  string cmd, color, pos, tmp; //cmd, color(black = 0), position
-	//loop for gtp input and output
-  while(cin >> cmd)
-  {
-		if(cmd == "play")
-		{
-			cin >> color >> pos;
-			bool c;
-			if(color[0] == 'b' || color[0] == 'B') c = BLACK; else c = WHITE;
-			env.add(GTP2Int(pos), c);
-			cout << '=' << endl << endl;
-		}
-		else if(cmd[0] == 'g' || cmd == "reg_genmove" )
-		{
-			cin >> color;
-			bool c;
-			if(color[0] == 'b' || color[0] == 'B') c = BLACK; else c = WHITE;
+    //some setting, initialization
+    MCTS agent;
+    board env;
+    env.clear();
+    srand(time(NULL));
+    int simu_per_step = DEFAUT_SIMS;
+    if (argc == 2) istringstream(string(argv[1])) >> simu_per_step;
+    cerr << "simulate " << simu_per_step << "/step" << endl;
+    string cmd, color, pos, tmp; //cmd, color(black = 0), position
+    //loop for gtp input and output
+    while (cin >> cmd) {
+        if (cmd == "play") {
+            cin >> color >> pos;
+            bool c;
+            if (color[0] == 'b' || color[0] == 'B') c = BLACK; else c = WHITE;
+            env.add(GTP2Int(pos), c);
+            cout << '=' << endl << endl;
+        }
+        else if (cmd[0] == 'g' || cmd == "reg_genmove") {
+            cin >> color;
+            bool c;
+            if (color[0] == 'b' || color[0] == 'B') c = BLACK; else c = WHITE;
 
-			//the agent make choices
-			Action a_max = agent.best_action(env, c, simu_per_step);
-			if(a_max == -1) {cout << "=resign" << endl << endl; continue;}
-			if(cmd[0] == 'g') env.add(a_max, c);
-			//note that the tree use board to search
-			//should always balck first or will be wrong adding
-			cout << '=' << Int2GTP(a_max) << endl << endl;
-		}
-		else if(cmd == "protocol_version") { cout << "=2" << endl << endl;}
-		else if(cmd == "name") {cout << "=" << NAME << endl << endl;}
-		else if(cmd == "version") {cout << "=" << VERSION << endl << endl;}
-		else if(cmd == "known_command")
-		{
-			string tmp;
-			cin >> tmp;
-			bool flag = 0;
-			for(int i = 0;i < 11; i++) if(tmp == known_commands[i]) {cout << "=" << true << endl << endl; flag = 1; break;}
-			if(!flag) {cout << "=" << false << endl << endl;}
-		}
-		else if(cmd == "list_commands")
-		{
-			cout << "=";
-			for(int i = 0;i < 11; i++) {cout << known_commands[i] << endl;}
-			cout << endl << endl;
-		}
-		else if(cmd == "boardsize") {cin >> tmp; cout << "=" << endl << endl; }
-		else if(cmd == "clear_board") {env.clear(); cout << "=" << endl << endl;}
-		else if(cmd == "komi") {cin >> tmp; cout << "=" << endl << endl;}
-		else if(cmd == "showboard" || cmd == "sb") //from hahanogo, debug usage
-		{
-			env.showboard();
-			cout<<endl;
-		}
-		else{ cout << "=" << endl << endl;}
-	}
-  //end loop for gtp input and output
+            //the agent make choices
+            Action a_max = agent.best_action(env, c, simu_per_step);
+            if (a_max == -1) {
+                cout << "=resign" << endl << endl;
+                continue;
+            }
+            if (cmd[0] == 'g')
+                env.add(a_max, c);
+            //note that the tree use board to search
+            //should always balck first or will be wrong adding
+            cout << '=' << Int2GTP(a_max) << endl << endl;
+        }
+        else if (cmd == "protocol_version") {
+            cout << "=2" << endl << endl;
+        }
+        else if (cmd == "name") {
+            cout << "=" << NAME << endl << endl;
+        }
+        else if (cmd == "version") {
+            cout << "=" << VERSION << endl << endl;
+        }
+        else if (cmd == "known_command") {
+            string tmp;
+            cin >> tmp;
+            bool flag = 0;
+            for (int i = 0; i < 11; i++) if (tmp == known_commands[i]) {
+                cout << "=" << true << endl << endl;
+                flag = 1;
+                break;
+            }
+            if (!flag) {
+                cout << "=" << false << endl << endl;
+            }
+        }
+        else if (cmd == "list_commands") {
+            cout << "=";
+            for (int i = 0; i < 11; i++) {
+                cout << known_commands[i] << endl;
+            }
+            cout << endl << endl;
+        }
+        else if (cmd == "boardsize") {
+            cin >> tmp;
+            cout << "=" << endl << endl;
+        }
+        else if (cmd == "clear_board") {
+            env.clear();
+            cout << "=" << endl << endl;
+        }
+        else if (cmd == "komi") {
+            cin >> tmp;
+            cout << "=" << endl << endl;
+        }
+        else if (cmd == "showboard" || cmd == "sb") //from hahanogo, debug usage
+        {
+            env.showboard();
+            cout << endl;
+        }
+        else {
+            cout << "=" << endl << endl;
+        }
+    }
+    //end loop for gtp input and output
 }
